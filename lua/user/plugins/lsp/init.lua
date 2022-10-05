3cd2ee60c531a4849b1dbda257561874df1451a6
felipeymn <felipe.ymm@gmail.com>
2022-10-05 08:17:29 -0300

    fix: enable null-ls after bugfix

--- lua/user/plugins/lsp/init.lua
+++ lua/user/plugins/lsp/init.lua
@@ -1,8 +1,8 @@
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-	return
+  return
 end
 
 require("user.plugins.lsp.configs")
 require("user.plugins.lsp.handlers").setup()
---[[ require("user.plugins.lsp.null_ls") ]]
+require("user.plugins.lsp.null_ls")
